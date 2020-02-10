ee31fa57a8cb769f88c600e3e74d6a128d4f07a2
Daniel Lewis <github@mrdaniellewis.co.uk>
2020-02-10 23:34:34 +0000

    wip

--- src/hooks/use_async_search.js
+++ src/hooks/use_async_search.js
@@ -1,9 +1,10 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo, useState, useRef } from 'react';
 import { optionise } from '../helpers/optionise.js';
 
 export const useAsyncSearch = (fn, { initialOptions = [] }) => {
   const [busy, setBusy] = useState(false);
   const [options, setOptions] = useState(initialOptions);
+  const lastSearchRef = useRef();
 
   const onSearch = useCallback(async (query) => {
     console.log('search', query);
@@ -11,10 +12,11 @@ export const useAsyncSearch = (fn, { initialOptions = [] }) => {
       setOptions(initialOptions);
       return;
     }
+    lastSearchRef.current = query;
     setBusy(true);
     const results = await fn(query);
     setBusy(false);
-    if (results === null) {
+    if (results === null || lastSearchRef.current !== query) {
       return;
     }
     setOptions(results);
