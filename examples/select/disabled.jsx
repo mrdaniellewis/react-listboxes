e0653c3b6bef7b5ee78e92bd98ead899c8da59c0
Daniel Lewis <github@mrdaniellewis.co.uk>
2020-03-14 11:36:01 +0000

    examples

---
+++ examples/select/disabled.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Select } from '../../src/components/select.jsx';
+
+const options = [
+  { label: 'Apple' },
+  { label: 'Banana', disabled: true },
+  { label: 'Orange' },
+];
+
+export function DisabledSelect() {
+  const [value, setValue] = useState(null);
+  return (
+    <>
+      <label htmlFor="disabled-select">
+        Select with disabled option
+      </label>
+      <Select
+        id="disabled-select"
+        value={value}
+        onValue={setValue}
+        options={options}
+      />
+    </>
+  );
+}
